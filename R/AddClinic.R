2c441102614d1a1d8fdb756c07b3b34b8bfab110
JRAnalytics <raffenne.jerome@gmail.com>
2023-02-24 11:32:13 +0100

    Add clinic MAJ

--- R/AddClinic.R
+++ R/AddClinic.R
@@ -2,59 +2,98 @@
 #'
 #' @param Metadata Meta object
 #' @param path dir path in which the GDC project is saved, or local files are saved
-#' @param name.local.file if loca=True, names to apply in Metadata object slot
-#' @param merge merge loaded data clinic with existing clinial data : cbind by rownames
+#' @param name if loca=True, names to apply in Metadata object slot
+#' @param merge merge loaded data clinic with existing clinial data : full_join by rownames
+#' @param name.local.file name file of interest in path directory, could be multiple names. c("a.csv","b.csv")
+#' @param mergeBy colname using for merging clinical data.
 #' @importFrom utils menu
+#' @import purrr
+#' @import dplyr
 #' @return a data.frame in the Meta Object
 #' @export
 #'
 #' @examples "none"
-AddClinic <- function(Metadata, path, merge = c(F,T), name.local.file) {
+AddClinic <- function(Metadata, path, merge = c(F,T), mergeBy, name, name.local.file = NULL) {
+
+  ### ecrasement si même nom dans le Meta à faire.
+
+
   if(!is.null(Metadata)){
+
     if(!is.list(Metadata)){
       stop("Metadata should be a list.")}
 
-    setwd(path)
-
       l <-length(names(Metadata))
-      lf <- list.files(path)
-      lf <- lf[str_detect(lf, ".clinic")]
 
-      if(all(str_detect(lf, ".rds", negate = FALSE)==F) & all(str_detect(lf, ".txt", negate = FALSE)==F) & all(str_detect(lf, "csv", negate = FALSE)==F) ){stop("No '*.rds' or '*.txt' '.csv' files in set directory. \n change path or add file")}
+      if(!is.null(name.local.file)){
+
+      filepath <- paste(path,name.local.file,sep="/")
+
+      if(length(filepath)>1){
+
+        if(is.null(mergeBy)){stop("For merging data from multiple loading, mergeBy='colnames' must be specified")}
+
+        clinic <- list()
+        count <- 0
+        for (i in filepath) {
+          if(all(str_detect(i, ".rds|.txt|.csv|.tsv", negate = FALSE)==F) ){stop("No '*.rds' or '*.txt' '.csv' files in set directory. \n change path or add file")}
+          count <- count+1
+
+          if(str_detect(i, ".rds", negate = FALSE)){
+            clinic[[count]] <- readRDS(i) }
+
+          if(str_detect(i, ".txt|.csv|.tsv", negate = FALSE)){
+
+            clinic[[count]] <- suppressWarnings(as.data.frame(data.table::fread(i, na.strings = "")))
+            rownames(clinic[[count]]) <-    clinic[[count]][,1]
+
+          }}
+
+          dt <- clinic %>% purrr::reduce(full_join, by=mergeBy)
+          rownames(dt) <- dt[,1]
 
 
-      if(str_detect(lf, ".rds", negate = FALSE)){Metadata[[l+1]] <- readRDS(lf) }
+        }} else {
 
-      if(str_detect(lf, ".txt|.csv|.tsv", negate = FALSE)){
+          LF <- list.files(path)
+          LF <- LF[str_detect(LF,"clinic")]
+          if(length(LF)>1){stop("Ther is more than one files with 'clinic' in its name. Switch to 'name.local.file' and 'mergeBy' ")}
 
-        dt <- suppressWarnings(as.data.frame(data.table::fread(lf, na.strings = "")))
+      if(all(str_detect(LF, ".rds|.txt|.csv|.tsv", negate = FALSE)==F)  ){stop("#55 No '*.rds' or '*.txt' '.csv' files in set directory. \n change path or add file")}
+
+
+      if(str_detect(LF, ".rds", negate = FALSE)){Metadata[[l+1]] <- readRDS(LF) }
+
+      if(str_detect(LF, ".txt|.csv|.tsv", negate = FALSE)){
+
+        dt <- suppressWarnings(as.data.frame(data.table::fread(LF, na.strings = "")))
         rownames(dt) <- dt[,1]
-        }
+        }}
+
 
         if (merge == F){
 
 
             Metadata[[l+1]] <- dt
 
-            names(Metadata)[l+1] <- name.local.file
+            names(Metadata)[l+1] <- name
         } else {
 
-          clinic <- Metadata[which(str_detect(names(Metadata),"clinic"))][[1]]
+          if(is.null(str_detect(names(Metadata),"clinic"))){stop("No clinical data found in Meta Object.\n
+                                                                 For merging 1 newly loaded clinical data to an already loaded file in Meta Object, firslty load one.\n
+                                                                 You can list a list of caracter for multiple clinical data to load at once and will be full_join.")}
 
-          clinic <- cbind(clinic, dt[rownames(clinic),])
+          if(is.null(mergeBy)){stop("For merging data, mergeBy='colnames' must be specified")}
+          clinic <- list(Metadata[which(str_detect(names(Metadata),"clinic"))][[1]],dt)
+
+          clinic <- clinic %>% purrr::reduce(full_join, by=mergeBy)
+
           Metadata[which(str_detect(names(Metadata),"clinic"))][[1]] <- clinic
-          }
+        }
 
       return(Metadata)
 
 
     }else{ stop("No meta data object found")
 
-
-
-
-  } #else meta data object not found
-
-
-
-} # function
+  } } # function
