d5b7c5397e444fb1a5f48d42a3b12d332ad2e8eb
JRAnalytics <raffenne.jerome@gmail.com>
2024-07-10 09:42:50 +0200

    Changing arboressence

--- R/CreateDataset.R
+++ R/CreateDataset.R
@@ -15,35 +15,38 @@ CreateDataset <- function(Project, path){
   file.path.parent <- paste(unlist(strsplit(path, "/")),collapse = "/")
   # Directories in "parent" Dir. Refereces, Processes, RawData and VerifiedDataSet
 
-       file.path.References <- paste(c(unlist(strsplit(path, "/")),"03References"),collapse = "/")
+       file.path.References <- paste(c(unlist(strsplit(path, "/")),"00References"),collapse = "/")
 
-      file.path.Processes <- paste(c(unlist(strsplit(path, "/")),"02Processes"),collapse = "/")
-            file.path.Project.Processes <- paste(c(file.path.Processes,project),collapse = "/")
+       file.path.Project.Processes <- paste(c(unlist(strsplit(path, "/")),Project,"02Processes"),collapse = "/")
 
-      file.path.RawData <- paste(c(unlist(strsplit(path, "/")),"01RawData"),collapse = "/")
-            file.path.Project.RawData <- paste(c(file.path.RawData,project),collapse = "/")
+       file.path.Project.RawData <- paste(c(unlist(strsplit(path, "/")),Project,"01RawData"),collapse = "/")
 
-      file.path.VerifiedDataSet <- paste(c(unlist(strsplit(path, "/")),"04VerifiedDataSet"),collapse = "/")
-            file.path.Project.VerifiedDataset <- paste(c(file.path.VerifiedDataSet,project),collapse = "/")
+       file.path.Project.VerifiedDataset <- paste(c(unlist(strsplit(path, "/")),Project,"03VerifiedDataSet"),collapse = "/")
 
-  if(!dir.exists(file.path.Processes)){dir.create(file.path.Processes)}
-  if(!dir.exists(file.path.RawData)){dir.create(file.path.RawData)}
+
+  #if(!dir.exists(file.path.Processes)){dir.create(file.path.Processes)}
+  #if(!dir.exists(file.path.RawData)){dir.create(file.path.RawData)}
+
+  if(!dir.exists(file.path(file.path.parent,Project))){dir.create(file.path(file.path.parent,Project))}
+
   if(!dir.exists(file.path.Project.RawData)){dir.create(file.path.Project.RawData)}
   if(!dir.exists(file.path.References)){dir.create(file.path.References)}
-  if(!dir.exists(file.path.VerifiedDataSet)){dir.create(file.path.VerifiedDataSet)}
+  #if(!dir.exists(file.path.VerifiedDataSet)){dir.create(file.path.VerifiedDataSet)}
+
   if(!dir.exists(file.path.Project.VerifiedDataset)){dir.create(file.path.Project.VerifiedDataset)}
+
   if(!dir.exists(file.path.Project.Processes)){dir.create(file.path.Project.Processes)}
 
 
 
 
   list.files.path <- list( "Parent"= file.path.parent,
-                           "RawData" = file.path.RawData,
+                           #"RawData" = file.path.RawData,
                            "Project.RawData" = file.path.Project.RawData,
-                           "Processes" = file.path.Processes,
+                           #"Processes" = file.path.Processes,
                            "Project.Processes" = file.path.Project.Processes,
                            "References"= file.path.References,
-                           "VerifiedDataSet" = file.path.VerifiedDataSet,
+                           #"VerifiedDataSet" = file.path.VerifiedDataSet,
                            "Project.VerifiedDataset" = file.path.Project.VerifiedDataset)
 
   message("Creating Project directories")
